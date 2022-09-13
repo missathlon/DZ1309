43c57d39d4e8724780d9b998301a7a4d1d5fdf8f
Ekaterina <missathlon08@gmail.com>
2022-09-13 16:08:02 +0300

    add Task 34

---
+++ task34/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+void FullArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+int Number(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) count++;
+    }
+    return count;
+}
+int[] array = new int[10];
+FullArray(array);
+Console.WriteLine("Kolichestvo chisel: " + Number(array));
